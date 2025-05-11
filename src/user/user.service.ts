8e7e4d16a08841088211d28807ee0dac179856db
aboodmokied <78992620+aboodmokied@users.noreply.github.com>
2025-05-12 01:35:59 +0300

    inject user model

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,4 +1,11 @@
 import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/sequelize';
+import { User } from './user.model';
 
 @Injectable()
-export class UserService {}
+export class UserService {
+    constructor(
+        @InjectModel(User)
+        private userModel: typeof User
+    ){}
+}
